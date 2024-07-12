3a34ca5d7116251dfe60921e6fd9be64d033fe70
Andrew <kylbitskiy2004@mail.ru>
2024-07-13 00:53:32 +0300

    Added EmailAlreadyExistsException

---
+++ src/main/java/training/web/dao/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package training.web.dao.exception;
+
+public class EmailAlreadyExistsException extends Exception{
+
+    private static final long serialVersionUID = 1L;
+
+    public EmailAlreadyExistsException (String message) {
+        super(message);
+    }
+}
