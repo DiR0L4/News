28e84b3cc500c70985174001fc987e51a87c6b93
Andrew <kylbitskiy2004@mail.ru>
2024-07-13 00:53:58 +0300

    Added connection pool initialization

--- src/main/java/training/web/controller/listener/Listener.java
+++ src/main/java/training/web/controller/listener/Listener.java
@@ -2,11 +2,18 @@ package training.web.controller.listener;
 
 import jakarta.servlet.ServletRequestEvent;
 import jakarta.servlet.ServletRequestListener;
+import jakarta.servlet.annotation.WebListener;
+import training.web.dao.connectionpool.ConnectionPool;
 
+@WebListener
 public class Listener implements ServletRequestListener {
     @Override
     public void requestInitialized(ServletRequestEvent sre) {
-        ServletRequestListener.super.requestInitialized(sre);
+        ConnectionPool.getInstance();
+    }
 
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+        ConnectionPool.getInstance().dispose();
     }
 }
