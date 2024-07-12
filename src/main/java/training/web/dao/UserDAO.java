338716558b0e5acfb7f06c7176a77e2fbdd2c62f
Andrew <kylbitskiy2004@mail.ru>
2024-07-13 00:55:26 +0300

    Added new trows exception to addUser()

--- src/main/java/training/web/dao/UserDAO.java
+++ src/main/java/training/web/dao/UserDAO.java
@@ -1,7 +1,8 @@
 package training.web.dao;
 
 import training.web.bean.RegistrationInfo;
+import training.web.dao.exception.EmailAlreadyExistsException;
 
 public interface UserDAO {
-    boolean addUser(RegistrationInfo regInfo) throws DAOException;
+    boolean addUser(RegistrationInfo regInfo) throws DAOException, EmailAlreadyExistsException;
 }
