0d73d1f4ec349dbaa1fadf748801fcd9c95f1327
Andrew <kylbitskiy2004@mail.ru>
2024-07-14 17:14:17 +0300

    Refactor

--- src/main/java/training/web/controller/listener/Listener.java
+++ src/main/java/training/web/controller/listener/Listener.java
@@ -1,19 +1,19 @@
 package training.web.controller.listener;
 
-import jakarta.servlet.ServletRequestEvent;
-import jakarta.servlet.ServletRequestListener;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 import training.web.dao.connectionpool.ConnectionPool;
 
 @WebListener
-public class Listener implements ServletRequestListener {
+public class Listener implements ServletContextListener {
     @Override
-    public void requestInitialized(ServletRequestEvent sre) {
+    public void contextInitialized(ServletContextEvent sce) {
         ConnectionPool.getInstance();
     }
 
     @Override
-    public void requestDestroyed(ServletRequestEvent sre) {
+    public void contextDestroyed(ServletContextEvent sce) {
         ConnectionPool.getInstance().dispose();
     }
 }
