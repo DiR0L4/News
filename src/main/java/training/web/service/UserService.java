1336f16743da0ed3261d6f42e349a91f5582fb00
Andrew <kylbitskiy2004@mail.ru>
2024-07-14 17:17:57 +0300

    Refactor

--- src/main/java/training/web/service/UserService.java
+++ src/main/java/training/web/service/UserService.java
@@ -3,8 +3,10 @@ package training.web.service;
 import training.web.bean.AuthInfo;
 import training.web.bean.RegistrationInfo;
 import training.web.bean.User;
+import training.web.dao.exception.EmailAlreadyExistsException;
+import training.web.service.exception.ValidationException;
 
 public interface UserService {
-    boolean registration(RegistrationInfo regInfo) throws ServiceException;
+    boolean registration(RegistrationInfo regInfo) throws ServiceException, EmailAlreadyExistsException, ValidationException;
     User authorization(AuthInfo authInfo) throws ServiceException;
 }
