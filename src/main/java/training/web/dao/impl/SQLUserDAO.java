ab85ef523d2739fbd56980c9d1d5fba6e44317d7
Andrew <kylbitskiy2004@mail.ru>
2024-07-14 17:16:53 +0300

    Added roleUsersInsert()

--- src/main/java/training/web/dao/impl/SQLUserDAO.java
+++ src/main/java/training/web/dao/impl/SQLUserDAO.java
@@ -16,15 +16,18 @@ public class SQLUserDAO implements UserDAO {
     private final static String USERS_ID_COLUMN = "id";
 
     private final static String INSERT_USER_SQL = "INSERT INTO users (login, password, email) VALUES (?, ?, ?)";
-    private final static String INSERT_ROLES_USERS_SQL = "INSERT INTO roles_users (users_id, roles_id) VALUES (?, ?)";
+
     @Override
     public boolean addUser(RegistrationInfo regInfo) throws DAOException, EmailAlreadyExistsException {
+        Connection connection = null;
+        PreparedStatement statementUsers = null;
+        ResultSet keys = null;
         try{
-            Connection connection = connectionPool.takeConection();
+            connection = connectionPool.takeConnection();
 
             emailUniquenessCheck(regInfo.getEmail());
 
-            PreparedStatement statementUsers = connection.prepareStatement(INSERT_USER_SQL, Statement.RETURN_GENERATED_KEYS);
+            statementUsers = connection.prepareStatement(INSERT_USER_SQL, Statement.RETURN_GENERATED_KEYS);
             statementUsers.setString(1, regInfo.getLogin());
             statementUsers.setString(2, regInfo.getPassword());
             statementUsers.setString(3, regInfo.getEmail());
@@ -33,39 +36,27 @@ public class SQLUserDAO implements UserDAO {
                 throw new SQLException("Creating user failed, no rows affected.");
             }
 
-            ResultSet keys = statementUsers.getGeneratedKeys();
+            keys = statementUsers.getGeneratedKeys();
             if(keys.next()){
-                int userId = (int) keys.getInt(USERS_ID_COLUMN);
+                int userId = keys.getInt(USERS_ID_COLUMN);
+                connectionPool.closeConnection(keys, statementUsers, connection);
                 userInfoInsert(userId, regInfo);
+                rolesUsersInsert(userId, regInfo);
             }
 
-            statementUsers.close();
-            keys.close();
-
-            PreparedStatement statementRolesUsers = connection.prepareStatement(INSERT_ROLES_USERS_SQL);
-            System.out.println("Statement roles_users created");
-            statementRolesUsers.setInt(1, userId);
-            statementRolesUsers.setInt(2, 2); // Добавить нормальный выбор роли
-
-            if (statementRolesUsers.executeUpdate() == 0) {
-                throw new SQLException("Creating roles_users failed, no rows affected.");
-            }
-            statementRolesUsers.close();
-
-            connection.close();
-            } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            return true;
+        } catch (SQLException e) {
             throw new DAOException(e);
         } catch (ConnectionPoolException e){
-            System.out.println(e.getMessage());
             throw new DAOException("Failed to open connection", e);
+        }finally {
+            connectionPool.closeConnection(keys, statementUsers, connection);
         }
-        return false;
     }
 
     private final static String SELECT_USER_ID_SQL = "SELECT id FROM users WHERE email = ?";
     private boolean emailUniquenessCheck(String email) throws EmailAlreadyExistsException, SQLException, ConnectionPoolException{
-        Connection connection = connectionPool.takeConection();
+        Connection connection = connectionPool.takeConnection();
         PreparedStatement statementUserId = connection.prepareStatement(SELECT_USER_ID_SQL);
         statementUserId.setString(1, email);
 
@@ -83,9 +74,8 @@ public class SQLUserDAO implements UserDAO {
         Connection connection = null;
         PreparedStatement statementUserInfo = null;
         try{
-            connection = connectionPool.takeConection();
+            connection = connectionPool.takeConnection();
             statementUserInfo = connection.prepareStatement(INSERT_USERINFO_SQL);
-            System.out.println("Statement userInfo created");
             statementUserInfo.setInt(1, userId);
             statementUserInfo.setString(2, regInfo.getCountry());
             statementUserInfo.setString(3, regInfo.getPhone());
@@ -102,6 +92,28 @@ public class SQLUserDAO implements UserDAO {
             throw new DAOException("Error occurred during inserting user info.", e);
         } finally {
             connectionPool.closeConnection(statementUserInfo, connection);
+        }
+    }
+
+    private final static String INSERT_ROLES_USERS_SQL = "INSERT INTO roles_users (users_id, roles_id) VALUES (?, ?)";
+    public boolean rolesUsersInsert(int userId, RegistrationInfo regInfo) throws DAOException{
+        Connection connection = null;
+        PreparedStatement statementRolesUsers = null;
+        try{
+            connection = connectionPool.takeConnection();
+            statementRolesUsers = connection.prepareStatement(INSERT_ROLES_USERS_SQL);
+            statementRolesUsers.setInt(1, userId);
+            statementRolesUsers.setInt(2, regInfo.getRoleId());
+
+            if (statementRolesUsers.executeUpdate() == 0) {
+                throw new SQLException("Creating roles_users failed, no rows affected.");
+            }
+
+            return true;
+        } catch (ConnectionPoolException | SQLException e){
+            throw new DAOException("Error occurred during inserting user info.", e);
+        } finally {
+            connectionPool.closeConnection(statementRolesUsers, connection);
         }
     }
 }
