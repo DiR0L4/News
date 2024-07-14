8bc96c90c93518bb4c7e513dbb94beb32fa67de8
Andrew <kylbitskiy2004@mail.ru>
2024-07-15 00:15:42 +0300

    Refactor

--- src/main/java/training/web/controller/concrete/impl/DoAuth.java
+++ src/main/java/training/web/controller/concrete/impl/DoAuth.java
@@ -7,36 +7,44 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import training.web.controller.concrete.Command;
-import training.web.logic.LogicStub;
 
 import training.web.bean.User;
 import training.web.bean.AuthInfo;
+import training.web.service.ServiceException;
+import training.web.service.ServiceProvider;
+import training.web.service.UserService;
+import training.web.service.exception.ValidationException;
 
 import java.io.IOException;
 
 public class DoAuth implements Command {
-    private final LogicStub logic = new LogicStub();
+    UserService userService = ServiceProvider.getInstance().getUserService();
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String login = request.getParameter("authEmail");
+        String email = request.getParameter("authEmail");
         String password = request.getParameter("authPassword");
 
-        User user = logic.checkAuth(new AuthInfo(login, password));
+        try{
+            User user = userService.authorization(new AuthInfo(email, password));
 
-        if(user != null) {
-            HttpSession session = (HttpSession) request.getSession(true);
-            session.setAttribute("user", user);
+            if(user != null) {
+                HttpSession session = (HttpSession) request.getSession(true);
+                session.setAttribute("user", user);
 
-            String rememberMe = request.getParameter("remember-me");
-            if (rememberMe != null) {
-                Cookie cookie = new Cookie("remember-me", user.getId()+"");
-                response.addCookie(cookie);
+                String rememberMe = request.getParameter("remember-me");
+                if (rememberMe != null) {
+                    Cookie cookie = new Cookie("remember-me", user.getId()+"");
+                    response.addCookie(cookie);
+                }
+                response.sendRedirect("MyController?command=go_to_main_page");
+
+            }else {
+                response.sendRedirect("MyController?command=go_to_auth&authError=Wrong email or password!");
             }
-
-            response.sendRedirect("MyController?command=go_to_main_page");
-
-        }else {
-            response.sendRedirect("MyController?command=go_to_auth&authError=Wrong email or password!");
+        }catch (ServiceException e){
+            response.sendRedirect("MyController?command=go_to_auth&authError=Error occurred during authorization");
+        }catch (ValidationException e){
+            response.sendRedirect("MyController?command=go_to_auth&authError=The fields are filled incorrectly");
         }
     }
 }
