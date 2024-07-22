44155a253587947d8857923f1fb03fc9b2f8872e
Andrew <kylbitskiy2004@mail.ru>
2024-07-22 19:54:53 +0300

    Refactor

--- src/main/java/training/web/controller/filter/RememberMeFilter.java
+++ src/main/java/training/web/controller/filter/RememberMeFilter.java
@@ -1,13 +1,17 @@
 package training.web.controller.filter;
 
 import jakarta.servlet.*;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpFilter;
-import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.*;
+import training.web.bean.User;
+import training.web.service.ServiceException;
+import training.web.service.ServiceProvider;
+import training.web.service.UserService;
 
 import java.io.IOException;
 
 public class RememberMeFilter extends HttpFilter implements Filter {
+
+    private final UserService userService = ServiceProvider.getInstance().getUserService();
 
     public RememberMeFilter() {
         super();
@@ -18,17 +22,31 @@ public class RememberMeFilter extends HttpFilter implements Filter {
 
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        Cookie[] cookies = ((HttpServletRequest)request).getCookies();
+        try {
+            HttpSession session = ((HttpServletRequest) request).getSession(false);
 
-        for(Cookie c : cookies) {
-            if (c.getName().equals("remember-me")) {
-                int userID = Integer.parseInt(c.getValue());
-                // мозговой штурм
+            if (session == null || session.getAttribute("user") == null) {
+                Cookie[] cookies = ((HttpServletRequest) request).getCookies();
+
+                if (cookies != null) {
+                    for (Cookie c : cookies) {
+                        if (c.getName().equals("remember-me")) {
+                            String token = c.getValue();
+                            User user = userService.rememberMe(token);
+                            session.setAttribute("user", user);
+                        }
+                    }
+                }
             }
+            chain.doFilter(request, response);
+
+        } catch (ServiceException e) {
+            ((HttpServletResponse) response)
+                    .sendRedirect("MyController?command=go_to_auth&authError=Wrong login or password!");
         }
 
-        chain.doFilter(request, response);
     }
+
 
     public void init(FilterConfig fConfig) throws ServletException {
 
